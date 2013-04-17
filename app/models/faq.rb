42e1397bfe83743a045946c6c2224c606654ed75
Caio <caiotuxo@gmail.com>
2013-04-17 17:09:51 -0300

    faqs

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,15 +1,15 @@
-# encoding: UTF-8
-class Faq < ActiveRecord::Base
-  attr_accessible :answer, :question, :subject_id, :subject
-  belongs_to :subject
+	# encoding: UTF-8
+	class Faq < ActiveRecord::Base
+	  attr_accessible :answer, :question, :subject_id, :subject
+	  belongs_to :subject
 
-  validates :question,
-    :presence => {:message => "Favor digitar uma pergunta"},
-    :uniqueness => {:message => "Essa pergunta já existe no sistema"},
-    :length => { :in => 5..255 , :message => "Deve que ter entre 5 e 255 caracteres"}
-  validates :answer, :presence => {:message => "Favor digitar uma resposta"}
-  validates :subject_id, :presence => {:message => "Favor digitar um assunto"}
+	  validates :question,
+	    :presence => {:message => "Favor digitar uma pergunta"},
+	    :uniqueness => {:message => "Essa pergunta já existe no sistema"},
+	    :length => { :in => 5..255 , :message => "Deve que ter entre 5 e 255 caracteres"}
+	  validates :answer, :presence => {:message => "Favor digitar uma resposta"}
+	  validates :subject_id, :presence => {:message => "Favor digitar um assunto"}
 
 
-  scope :sorted, order('faqs.subject_id ASC')
-end
+	  scope :sorted, order('faqs.subject_id ASC')
+	end
